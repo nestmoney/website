b5d90038072cd47cce679aee24305dfb23691ceb
Dhanush <iamdk353@gmail.com>
2026-02-07 11:30:25 +0530

    ui:added hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,4 +1,24 @@
+import Image from "next/image";
+import DownloadBtn from "../DownloadButton";
+
 const Hero = () => {
-  return <div className="min-h-screen pt-[73px]">Hero</div>;
+  return (
+    <div className="h-screen py-[73px]">
+      <div className="h-[90%] flex flex-col items-center">
+        <p className="font-bold text-[48px] text-secondary pb-[32px]">
+          A home for your{" "}
+          <span className="text-primary-blue">family finances</span>
+        </p>
+        <DownloadBtn />
+        <Image
+          src={"/dummy.svg"}
+          width={960}
+          height={330}
+          alt="hero image "
+          className="pt-[80px]"
+        ></Image>
+      </div>
+    </div>
+  );
 };
 export default Hero;
