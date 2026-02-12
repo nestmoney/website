9717b5fe3d29056f5c44274dba577e641b36ccb5
supreethts <supreethts23@gmail.com>
2026-02-13 00:13:00 +0530

    footer left padding remove

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,7 +5,7 @@ import Partners from "@/components/sections/Partners";
 
 export default function Main() {
   return (
-    <main className="site-container mx-auto  space-y-[64px]">
+    <main className="site-container mx-auto  space-y-[80px]">
       <Hero />
       <About />
       <Features />
