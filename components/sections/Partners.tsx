983a1cc53bd7b802cf338335eaa9921a1e6ef48f
supreethts <supreethts23@gmail.com>
2026-02-18 23:39:31 +0530

    Partners spacing

--- components/sections/Partners.tsx
+++ components/sections/Partners.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Partners = () => {
   return (
     <section
-      className="flex justify-center items-center pt-[32px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px]"
+      className="flex justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px]"
       aria-labelledby="partners-title"
     >
       <h2 id="partners-title" className="sr-only">
