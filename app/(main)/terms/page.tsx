dd2fcffcf5c7b855779ce30e4d99abb93eea134b
Dhanush <iamdk353@gmail.com>
2026-02-18 17:15:39 +0530

    tweak on term of use

--- app/(main)/terms/page.tsx
+++ app/(main)/terms/page.tsx
@@ -18,7 +18,7 @@ const page = () => {
   return (
     <main className="min-h-screen py-[75px] site-container mx-auto text-justify ">
       <header className="h-[70px] md:h-[152px] text-[20px] md:text-[40px] font-semibold items-center bg-primary-blue text-white bg-[url('/linksPattren.svg')] bg-cover bg-center bg-no-repeat flex scroll-mt-[80px] pl-6 md:pl-10 md:rounded-tl-[40px] md:rounded-br-[40px] mb-10 rounded-tl-[24px] rounded-br-[24px]">
-        <h1>Terms Of Use</h1>
+        <h1>Terms of Use</h1>
       </header>
       <Para>
         Welcome to Nest Money! The Nest Money Mobile Application, the website
