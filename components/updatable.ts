01d1b5ed75327993b587bb539efd25cd93c08cce
Dhanush <iamdk353@gmail.com>
2026-02-19 13:47:04 +0530

    REMOVED google and apple links

--- components/updatable.ts
+++ components/updatable.ts
@@ -1,71 +1,8 @@
 export const Links = {
-  appStore: "https://apps.apple.com/",
-  playStore: "https://play.google.com/store",
+  appStore: "",
+  playStore: "",
   instagram: "",
   linkedin: "",
   facebook: "",
   x: "",
 };
-
-export const experianData = [
-  {
-    title: "Section 1: General",
-    items: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-    ],
-  },
-  {
-    title: "Section 2: Payments",
-    items: [
-      "Payment must be completed before project delivery.",
-      "Late payments may incur additional charges.",
-    ],
-  },
-  {
-    title: "Section 1: General",
-    items: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-    ],
-  },
-  {
-    title: "Section 2: Payments",
-    items: [
-      "Payment must be completed before project delivery.",
-      "Late payments may incur additional charges.",
-    ],
-  },
-  {
-    title: "Section 1: General",
-    items: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-    ],
-  },
-  {
-    title: "Section 2: Payments",
-    items: [
-      "Payment must be completed before project delivery.",
-      "Late payments may incur additional charges.",
-    ],
-  },
-  {
-    title: "Section 1: General",
-    items: [
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit..Lorem ipsum dolor sit amet consectetur adipisicing elit...Lorem ipsum dolor sit amet consectetur adipisicing elit...Lorem ipsum dolor sit amet consectetur adipisicing elit...Lorem ipsum dolor sit amet consectetur adipisicing elit...Lorem ipsum dolor sit amet consectetur adipisicing elit...Lorem ipsum dolor sit amet consectetur adipisicing elit....",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
-    ],
-  },
-  {
-    title: "Section 2: Payments",
-    items: [
-      "Payment must be completed before project delivery.",
-      "Late payments may incur additional charges.",
-    ],
-  },
-];
