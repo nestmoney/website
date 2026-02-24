925c143484095c12fe612775132223627f45af53
supreethts <supreethts23@gmail.com>
2026-02-25 02:00:26 +0530

    Revert "India image"
    This reverts commit 07590cb185fd3d5fa477a64d786762f2ec19e5e7.

--- components/sections/Partners.tsx
+++ components/sections/Partners.tsx
@@ -3,18 +3,12 @@ import Image from "next/image";
 const Partners = () => {
   return (
     <section
-      className="flex flex-col gap-[20px] md:gap-[32px] justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px] w-[320px] md:w-full mx-auto"
+      className="flex justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px] w-[320px] md:w-full mx-auto"
       aria-labelledby="partners-title"
     >
-      <figure>
-        <Image
-          src="/india_image.png"
-          alt="Partner logos"
-          width={600}
-          height={90}
-        />
-      </figure>
-
+      <h2 id="partners-title" className="sr-only">
+        Partners
+      </h2>
       <figure>
         <Image
           src="/partners.svg"
