34c1803d17c6e38505072ec1ad9a8f9d0d20ac24
supreethts <supreethts23@gmail.com>
2026-02-25 02:28:02 +0530

    padding - india image

--- components/sections/Partners.tsx
+++ components/sections/Partners.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Partners = () => {
   return (
     <section
-      className="flex flex-col gap-[20px] md:gap-[36px]justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px] w-[320px] md:w-full mx-auto"
+      className="flex flex-col justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px] w-[320px] md:w-full mx-auto"
       aria-labelledby="partners-title"
     >
       <figure>
@@ -12,6 +12,7 @@ const Partners = () => {
           alt="India Image"
           width={600}
           height={90}
+          className="pb-[18px] md:pb-[34px]"
         />
       </figure>
 
