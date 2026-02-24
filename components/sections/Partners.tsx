6c3d3f38eb0006439a94a10f1972ea122dd4e82c
supreethts <supreethts23@gmail.com>
2026-02-25 02:03:47 +0530

    India image

--- components/sections/Partners.tsx
+++ components/sections/Partners.tsx
@@ -3,12 +3,18 @@ import Image from "next/image";
 const Partners = () => {
   return (
     <section
-      className="flex justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px] w-[320px] md:w-full mx-auto"
+      className="flex flex-col gap-[20px] md:gap-[32px]justify-center items-center pt-[64px] md:pt-[32px] xl:pt-[40px] mb-[64px] md:mb-[128px] xl:mb-[160px] w-[320px] md:w-full mx-auto"
       aria-labelledby="partners-title"
     >
-      <h2 id="partners-title" className="sr-only">
-        Partners
-      </h2>
+      <figure>
+        <Image
+          src="/india_image.png"
+          alt="India Image"
+          width={600}
+          height={90}
+        />
+      </figure>
+
       <figure>
         <Image
           src="/partners.svg"
