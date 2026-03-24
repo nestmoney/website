75d08fd74f579979fffac2be76eb7ba4c10302fc
supreethts <supreethts23@gmail.com>
2026-03-24 14:34:30 +0530

    App store link update

--- components/updatable.ts
+++ components/updatable.ts
@@ -1,5 +1,5 @@
 export const Links = {
-  appStore: "https://apps.apple.com/app/nestmoney/id6756575032",
+  appStore: "https://apps.apple.com/in/app/nest-money/id6756575032",
   playStore: "https://play.google.com/store/apps/details?id=in.legentree.nestmoney",
   instagram: "",
   linkedin: "",
